af1f0def70694b4dd4b5ed5fe4e6d2676f8a18b6
Jedidiah <152191672+shampeyndadi@users.noreply.github.com>
2024-08-11 22:37:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const x = document.getElementById("Github");
+
+function goToProfileGit(){
+    window.open("https://github.com/shampeyndadi", "_blank");
+}
+
+x.addEventListener("click", goToProfileGit);
